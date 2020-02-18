2cbdeb50f792b3d20563a2e201432729be270b69
Tkachuk Alexandr <tkachukalexandr62@gmail.com>
2020-02-18 19:59:10 +0200

    create a test chart

--- Dashboard/src/app/app.module.ts
+++ Dashboard/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {ChartsModule} from 'ng2-charts';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
@@ -11,6 +12,9 @@ import { SeactionHealthComponent } from './layouts/seaction-health/seaction-heal
 import { LoginComponent } from './auth/login/login.component';
 import { RegistrComponent } from './auth/registr/registr.component';
 import { RouterModule } from '@angular/router';
+import { BarChartComponent } from './layouts/bar-chart/bar-chart.component';
+import { LineChartComponent } from './layouts/line-chart/line-chart.component';
+import { PieChartComponent } from './layouts/pie-chart/pie-chart.component';
 
 @NgModule({
   declarations: [
@@ -21,11 +25,15 @@ import { RouterModule } from '@angular/router';
     SectionOrdersComponent,
     SeactionHealthComponent,
     LoginComponent,
-    RegistrComponent
+    RegistrComponent,
+    BarChartComponent,
+    LineChartComponent,
+    PieChartComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    ChartsModule
   ],
   providers: [
   ],
